1cc1d61bbf74430c6ba8a846c6cc44fb09212a53
jonny kang <jono.kang@gmail.com>
2014-08-01 19:17:09 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZenPresoRails::Application.config.secret_key_base = '6f205ea141e2d4e7912d7cfc0560ef08ee61a8d836c74c1eb765b6bceda851c205f743e13011f7e5b4571f283e96ba19f24a153090edc9e16f23cc735067075e'
